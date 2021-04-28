7e4046a459a1ec85bad5eab4089bfce4cc838fcd
X0SS <jupi4r@gmail.com>
2021-04-28 12:06:10 +0700

    git add get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,12 +1,18 @@
-def main:
-  print("Keep it logically awesome.")
+import random
+def main():
+  #print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #print(quotes)
+  print(quotes[0])
 
-if __name__== "main":
-  main
+  last = 13
+  rnd = random.randint(0, 13)
+  
+  print(quotes[rnd])
 
+if __name__== "__main__":
+  main()
+  
